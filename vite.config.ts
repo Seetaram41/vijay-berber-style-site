06dc9c588ac413d5081bfb56fc156bea57238f0d
Seeta Ram <Seeta Ram@SEETARAMNAGORE>
2025-06-26 21:20:51 +0530

    vijay-berber-style-site/  add this into vite.config file

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-  base: mode === 'production' ? '/vijay-hair-dresser/' : '/',
+  base: mode === 'production' ? '/vijay-berber-style-site/' : '/',
   plugins: [
     react(),
     mode === 'development' &&
